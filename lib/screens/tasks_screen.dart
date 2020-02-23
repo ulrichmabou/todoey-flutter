87bac861dd58df03de459efd189fb0b2e39d79c8
UlrichMabou <ulrichmabou@gmail.com>
2020-02-23 16:49:32 +0300

    position the BottomSheet just above the keyboard

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -12,7 +12,16 @@ class TasksScreen extends StatelessWidget {
         child: Icon(Icons.add),
         onPressed: () {
           showModalBottomSheet(
-              context: context, builder: (context) => AddTaskScreen());
+            context: context,
+            isScrollControlled: true,
+            builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskScreen(),
+              ),
+            ),
+          );
         },
       ),
       body: Column(
